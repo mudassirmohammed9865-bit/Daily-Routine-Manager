69e99d165d2f341acb32d53431b4aef1e6d7d486
Mohammed Mudassir <mudassirmohammed9865@gmail.com>
2026-05-03 15:46:06 +0530

    Serve frontend

--- backend/server.js
+++ backend/server.js
@@ -40,3 +40,12 @@ app.delete("/delete/:id", async (req, res) => {
 app.listen(5000, () =>
   console.log("Server running on port 5000")
 );
+const path = require("path");
+
+// Serve frontend folder
+app.use(express.static(path.join(__dirname, "../frontend")));
+
+// Default route
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "../frontend/index.html"));
+});
